09e1468e86a56c9810a79c5315305ad895af9aba
Анна Стріхарчук <stannapav@gmail.com>
2023-02-20 12:46:06 +0200

    final ver single linked list

--- LinkedLists.h
+++ LinkedLists.h
@@ -5,11 +5,10 @@
 template<typename T>
 class ListSingle {
 private:
-	
+	NodeSingle<T>* head;
 	int size;
 
 public:
-	NodeSingle<T>* head;
 	ListSingle();
 	void print();
 	void addFront(T data);
@@ -24,10 +23,10 @@ public:
 	
 };
 
-//template<typename T>
-//class ListDouble {
-//
-//};
+template<typename T>
+class ListDouble {
+
+};
 
 template<typename T>
 ListSingle<T>::ListSingle(){
@@ -86,7 +85,7 @@ void ListSingle<T>::addAtPosition(int index, T data){
 	}
 
 	NodeSingle<T>* last = this->head;
-	for (int i = 0; last->next != NULL && i < index; i++) {
+	for (int i = 0; last->next->next != NULL && i < index; i++) {
 		last = last->next;
 	}
 
@@ -98,27 +97,67 @@ void ListSingle<T>::addAtPosition(int index, T data){
 }
 
 template<typename T>
-void ListSingle<T>::changeData(int index, T data){
+void ListSingle<T>::changeData(int index, T changeData){
+	if (this->size <= index) {
+		cout << "bad index";
+		return;
+	}
 
+	NodeSingle<T>* temp = this->head;
+	for (int i = 0; temp->next != NULL && i < index; i++) {
+		temp = temp->next;
+	}
+	
+	temp->data = changeData;
 }
 
 template<typename T>
 void ListSingle<T>::delFront(){
+	if (this->head != NULL) {
+		NodeSingle<T>* temp = this->head->next;
 
+		delete this->head;
+
+		this->head = temp;
+	}
 }
 
 template<typename T>
 void ListSingle<T>::delBack(){
+	if (this->head == NULL)
+		return;
 
+	NodeSingle<T>* last = this->head;
+	while (last->next->next != NULL)
+		last = last->next;
+
+	delete last->next;
+	last->next = NULL;
 }
 
 template<typename T>
 void ListSingle<T>::delAll(){
+	if (this->head == NULL)
+		return;
 
+	NodeSingle<T>* nextNode = NULL;
+	while (this->head != NULL) {
+		nextNode = this->head->next;
+		delete this->head;
+		this->head = nextNode;
+	}
 }
 
 template<typename T>
 NodeSingle<T>& ListSingle<T>::getAt(int index){
+	if (this->size <= index) {
+		throw("Bad index");
+	}
 
-	return NodeSingle<T>();
+	NodeSingle<T>* last = this->head;
+	for (int i = 0; last->next != NULL && i < index; i++) {
+		last = last->next;
+	}
+
+	return *last;
 }
